67590c552e2a3aa4c405057a4a30e897d44652f0
tumble <tumblegamer@gmail.com>
2023-07-05 12:34:19 +0100

    sky remote

---
+++ sky-remote.user.js
@@ -0,0 +1,72 @@
+
+
+// ==UserScript==
+// @name         STBG Sky Remote
+// @namespace    https://stb-gaming.github.io
+// @version      0.0.1
+// @description  Creates fucntions to press sky remote buttons
+// @author       You
+// @run-at       document-start
+// @match        https://denki.co.uk/sky/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=denki.co.uk
+// ==/UserScript==
+
+
+(function () {
+	'use strict';
+	const uWindow = typeof unsafeWindow != 'undefined' ? unsafeWindow : window;
+
+	let remote = {
+		"sky": 27,
+		"tv-guide": 65,
+		"box-office": 83,
+		"services": 68,
+		"interactive": 70,
+		"i": 71,
+		"Up": 73,
+		"Left": 74,
+		"down": 75,
+		"right": 76,
+		"select": 13,
+		"channel-up": 33,
+		"channel-down": 34,
+		"backup": 8,
+		"help": 84,
+		"red": 81,
+		"green": 87,
+		"yellow": 69,
+		"blue": 82,
+		"0": 48,
+		"1": 49,
+		"2": 50,
+		"3": 51,
+		"4": 52,
+		"5": 53,
+		"6": 54,
+		"7": 55,
+		"8": 56,
+		"9": 57,
+	};
+
+	function triggerEvent(event, key) {
+		window.dispatchEvent(new KeyboardEvent(event, {
+			keyCode: key
+		}));
+	}
+
+	uWindow.SkyRemote = {
+		listButtons() {
+			return Object.keys(remote);
+		},
+		keyDown(key) {
+			triggerEvent("keydown", remote[key]);
+		},
+		keyUp(key) {
+			triggerEvent("keyup", remote[key]);
+		},
+		pressKey(key) {
+			this.keyDown(key);
+			setTimeout(() => this.keyUp(key), 500);
+		}
+	};
+})();
