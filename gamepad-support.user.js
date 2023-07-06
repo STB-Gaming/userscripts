0f6ee20d756eb828f5f815a50645c5fb337a2a2e
cobaltgit <cobaltmail4204@gmail.com>
2023-07-06 13:34:01 +0100

    Allow multiple inputs to be processed better

--- gamepad-support.user.js
+++ gamepad-support.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         STBG Controller Support Script
 // @namespace    https://stb-gaming.github.io
-// @version      0.1.1
+// @version      0.1.2
 // @description  A script that uses the JS Gamepad API to add controller support to Denki's online Sky Games
 // @author       cobaltgit
 // @run-at       document-start
@@ -18,7 +18,7 @@
     let
         gamepads,
         start,
-        buttonNumbers = [73, 75, 76, 74, 8, 13, 81, 82, 69, 87, 84]
+        buttonNumbers = [12, 13, 15, 14, 9, 0, 1, 2, 3, 8, 11]
 
     function mainLoop() {
         gamepads = navigator.getGamepads();
@@ -27,43 +27,43 @@
         const gamepad = gamepads[0];
         const buttons = gamepad.buttons;
 
-        switch (true) {
-            case buttons[12].pressed:
-                SkyRemote.holdButton("up"); // d-pad up
-                break;
-            case buttons[13].pressed:
-                SkyRemote.holdButton("down"); // d-pad down
-                break;
-            case buttons[15].pressed:
-                SkyRemote.holdButton("right"); // d-pad right
-                break;
-            case buttons[14].pressed:
-                SkyRemote.holdButton("left"); // d-pad left
-                break;
-            case buttons[9].pressed:
-                SkyRemote.holdButton("backup"); // start (back up)
-                break;
-            case buttons[0].pressed:
-                SkyRemote.holdButton("select"); // A (select)
-                break;
-            case buttons[1].pressed:
-                SkyRemote.holdButton("red"); // B (red)
-                break;
-            case buttons[2].pressed:
-                SkyRemote.holdButton("blue"); // X (blue)
-                break;
-            case buttons[3].pressed:
-                SkyRemote.holdButton("yellow"); // Y (yellow)
-                break;
-            case buttons[8].pressed:
-                SkyRemote.holdButton("green"); // select (green)
-                break;
-            case buttons[11].pressed:
-                SkyRemote.holdButton("help"); // right stick down (help)
-                break;
-            default:
-                SkyRemote.listButtons().forEach(button => SkyRemote.releaseButton(button));
+        if (buttons[12].pressed) {
+            SkyRemote.holdButton("up"); // d-pad up
+        } 
+        if (buttons[13].pressed) {
+            SkyRemote.holdButton("down"); // d-pad down
         }
+        if (buttons[15].pressed) {
+            SkyRemote.holdButton("right"); // d-pad right
+        }
+        if (buttons[14].pressed) {
+            SkyRemote.holdButton("left"); // d-pad left
+        }
+        if (buttons[9].pressed) {
+            SkyRemote.pressButton("backup"); // start (back up)
+        }
+        if (buttons[0].pressed) {
+            SkyRemote.pressButton("select"); // A (select)
+        }
+        if (buttons[1].pressed) {
+            SkyRemote.pressButton("red"); // B (red)
+        }
+        if (buttons[2].pressed) {
+            SkyRemote.pressButton("blue"); // X (blue)
+        }
+        if (buttons[3].pressed) {
+            SkyRemote.pressButton("yellow"); // Y (yellow)
+        }
+        if (buttons[8].pressed) {
+            SkyRemote.pressButton("green"); // select (green)
+        }
+        if (buttons[11].pressed) {
+            SkyRemote.pressButton("help"); // right stick down (help)
+        }
+        if (!buttonNumbers.some(index => buttons[index].pressed)) {
+            SkyRemote.listButtons().forEach(button => SkyRemote.releaseButton(button));
+        }
+        
 
         start = uWindow.requestAnimationFrame(mainLoop)
     }
