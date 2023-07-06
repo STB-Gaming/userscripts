87932feb09c75c3cd58a76c347ccb8790a11f42a
tumble <tumblegamer@gmail.com>
2023-07-06 22:39:05 +0100

    added support for epg menus

--- gamepad-support.user.js
+++ gamepad-support.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         STBG Controller Support Script
 // @namespace    https://stb-gaming.github.io
-// @version      0.2.0
+// @version      0.2.1
 // @description  A script that uses the JS Gamepad API to add controller support to Denki's online Sky Games
 // @author       cobaltgit
 // @run-at       document-start
 // @match        https://denki.co.uk/sky/*
+// @match        https://stb-gaming.github.io/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=denki.co.uk
 // @require      https://github.com/STB-Gaming/userscripts/raw/master/sky-remote.user.js
 // ==/UserScript==
@@ -13,46 +14,46 @@
 
 (function () {
 	'use strict';
-	const 
-        uWindow = typeof unsafeWindow != 'undefined' ? unsafeWindow : window,
-        buttonMapping = {
-            12: "up",
-            13: "down",
-            14: "left",
-            15: "right",
-            9: "backup",
-            0: "select",
-            1: "red",
-            2: "blue",
-            3: "yellow",
-            8: "green",
-            11: "help"
-        }
+	const
+		uWindow = typeof unsafeWindow != 'undefined' ? unsafeWindow : window,
+		buttonMapping = {
+			12: "up",
+			13: "down",
+			14: "left",
+			15: "right",
+			9: "backup",
+			0: "select",
+			1: "red",
+			2: "blue",
+			3: "yellow",
+			8: "green",
+			11: "help"
+		};
 
-    let start, gamepad;
+	let start, gamepad;
 
-    function mainLoop() { // todo: non-linear input handling?
-        let gamepads = navigator.getGamepads();
-        if (!gamepads) return;
-        gamepad = gamepads[0];
+	function mainLoop() { // todo: non-linear input handling?
+		let gamepads = navigator.getGamepads();
+		if (!gamepads) return;
+		gamepad = gamepads[0];
 
-        for (const index of Object.keys(buttonMapping)) {
-            if (gamepad.buttons[index].pressed) {
-                SkyRemote.holdButton(buttonMapping[index]);
-                break;
-            } else {
-                SkyRemote.releaseButton(buttonMapping[index]);
-            }
-        }
-        
-        start = uWindow.requestAnimationFrame(mainLoop)
-    }
+		for (const index of Object.keys(buttonMapping)) {
+			if (gamepad.buttons[index].pressed) {
+				SkyRemote.holdButton(buttonMapping[index]);
+				break;
+			} else {
+				SkyRemote.releaseButton(buttonMapping[index]);
+			}
+		}
 
-    uWindow.addEventListener("gamepadconnected", event => {
-        mainLoop();
-    });
+		start = uWindow.requestAnimationFrame(mainLoop);
+	}
 
-    uWindow.addEventListener("gamepaddisconnected", event => {
-        uWindow.cancelAnimationFrame(start);
-    });
+	uWindow.addEventListener("gamepadconnected", event => {
+		mainLoop();
+	});
+
+	uWindow.addEventListener("gamepaddisconnected", event => {
+		uWindow.cancelAnimationFrame(start);
+	});
 })();
