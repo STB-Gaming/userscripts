b82711502a687cadea720960cf7deb50be470a29
tumble <tumblegamer@gmail.com>
2023-07-10 17:50:25 +0100

    fix button names

--- beehive-bedlam.user.js
+++ beehive-bedlam.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         STBG Beehive Bedlam
 // @namespace    https://stb-gaming.github.io
-// @version      0.1.4
+// @version      0.1.5
 // @description  A userscript that makes the online Beehive Bedlam remake compatible with STBG's standardised controls
 // @author       tumble1999
 // @run-at       document-start
@@ -317,7 +317,7 @@
 			SkyRemote.onReleaseButton("left", pressLeft);
 			SkyRemote.onReleaseButton("right", pressRight);
 			SkyRemote.onReleaseButton("select", pressSelect);
-			SkyRemote.onReleaseButton("back", pressBack);
+			SkyRemote.onReleaseButton("backup", pressBack);
 		}
 
 	});
